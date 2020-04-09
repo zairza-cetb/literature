20f26730d0e4f93ac99c3e7836bb2cda8d857113
amgugu111 <amgugu111@gmail.com>
2020-04-09 05:45:30 +0530

    Modified the homepage.dart and the creategame.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: LiteratureHomePage(title: 'Literature'),
+      home: LiteratureHomePage(),
     );
   }
 }
