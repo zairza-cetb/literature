b3dbd6d31371c0a6d89b3f2355d4eb6ebda23162
Aquib Baig <aquibbaig97@gmail.com>
2020-04-12 11:30:42 +0530

    Go to next screen if clicked on text too, indentation fixes

--- lib/screens/creategame.dart
+++ lib/screens/creategame.dart
@@ -32,141 +32,167 @@ class CreateGame extends StatelessWidget {
           )
         ],
       ),
-        body: ListView(children: [
-          Stack(children: [
-            Container(
-              height: MediaQuery.of(context).size.height - 82,
-              width: MediaQuery.of(context).size.width,
-              color: Colors.transparent),
-            Positioned(
-              top: 75.0,
-              child: Container(
-                decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(45.0),
-                  topRight: Radius.circular(45.0),
-
-                  ),
-                  color: Colors.white),
-              height: MediaQuery.of(context).size.height - 100.0,
-              width: MediaQuery.of(context).size.width)),
-            Positioned(
+      body: ListView(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height - 82,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.transparent
+              ),
+              Positioned(
+                top: 75.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(45.0),
+                        topRight: Radius.circular(45.0),
+                      ),
+                      color: Colors.white
+                    ),
+                  height: MediaQuery.of(context).size.height - 100.0,
+                  width: MediaQuery.of(context).size.width
+                )
+              ),
+              Positioned(
                 top: -30.0,
                 left: (MediaQuery.of(context).size.width / 2) - 200.0,
                 child: Container(
                   decoration: BoxDecoration(
                     image: DecorationImage(
                       image: AssetImage('assets/create_room_bg.png'),
-                      fit: BoxFit.cover)),
-                        height: 600.0,
-                        width: 400.0)
-            ),
-            Positioned(
-              top: 470,
-              left: 25.0,
-              right: 25.0,
-              child: Container(
-              padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                child: Column(
-              children: <Widget>[
-
-                  SizedBox(height: 40.0),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => CreateRoom()),
-                      );
-                    },
-                    child: Container(
-                      height: 50.0,
-                      child: Material(
-                        borderRadius: BorderRadius.circular(20.0),
-                        shadowColor: Color(0xFFb3e5fc),
-                        color: Color(0xFF039be5),
-                        elevation: 7.0,
-                        child: GestureDetector(
-                        onTap: () {},
-                          child: Center(
-                            child: Text(
-                            'Create Room',
-                            style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Montserrat'),
-                          ),
-                        ),
-                      ),
+                      fit: BoxFit.cover
                     ),
-                  )),
-                  SizedBox(height: 20.0),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => JoinRoom()),
-                      );
-                    },
-                    child: Container(
-                      height: 50.0,
-                      child: Material(
-                        borderRadius: BorderRadius.circular(20.0),
-                        shadowColor: Color(0xFF24315E),
-                        color: Color(0xffb0bec5),
-                        elevation: 7.0,
-                        child: GestureDetector(
-                          onTap: () {},
-                          child: Center(
-                            child: Text(
-                              'Join Room',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Montserrat'),
+                  ),
+                  height: 600.0,
+                  width: 400.0
+                ),
+              ),
+              Positioned(
+                top: 470,
+                left: 25.0,
+                right: 25.0,
+                child: Container(
+                  padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 40.0),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => CreateRoom()),
+                          );
+                        },
+                        child: Container(
+                          height: 50.0,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20.0),
+                            shadowColor: Color(0xFFb3e5fc),
+                            color: Color(0xFF039be5),
+                            elevation: 7.0,
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => CreateRoom()),
+                                );
+                              },
+                              child: Center(
+                                child: Text(
+                                  'Create Room',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat'
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    )),
-                  SizedBox(height: 50.0),
-                  InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => JoinRoom()),
-                      );
-                    },
-                    child:Container(
-                      height: 40.0,
-                      color: Colors.transparent,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                            style: BorderStyle.solid,
-                            width: 1.0),
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(20.0)),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                          Center(
-                          child:
-                            ImageIcon(AssetImage('assets/facebook.png')),
+                      SizedBox(height: 20.0),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => JoinRoom()),
+                          );
+                        },
+                        child: Container(
+                          height: 50.0,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20.0),
+                            shadowColor: Color(0xFF24315E),
+                            color: Color(0xffb0bec5),
+                            elevation: 7.0,
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => JoinRoom()),
+                                );
+                              },
+                              child: Center(
+                                child: Text(
+                                  'Join Room',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat'),
+                                ),
+                              ),
                             ),
-                            SizedBox(width: 10.0),
-                          Center(
-                            child: Text('Invite Friends with Facebook',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Montserrat')),
-                      )
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 50.0),
+                      InkWell(
+                        onTap: () {
+                          print("Invite friends from FaceBook...");
+                        },
+                        child:Container(
+                          height: 40.0,
+                          color: Colors.transparent,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                style: BorderStyle.solid,
+                                width: 1.0
+                              ),
+                              color: Colors.transparent,
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Center(child:
+                                  ImageIcon(AssetImage('assets/facebook.png'))
+                                ),
+                                SizedBox(width: 10.0),
+                                Center(
+                                  child: Text('Invite Friends with Facebook',
+                                    style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat')
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
-              ))
-            ])),),
-          ])])
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
