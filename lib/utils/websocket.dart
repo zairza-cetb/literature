cd0b31369d1603f8d2394e9e4fd87b6c2574ae97
Aquib Baig <aquibbaig97@gmail.com>
2020-04-13 23:52:36 +0530

    Using socket_io_client instead of websockets_channel (#23)

--- lib/utils/websocket.dart
+++ lib/utils/websocket.dart
@@ -1,93 +1,67 @@
 import 'package:flutter/foundation.dart';
-import 'package:web_socket_channel/io.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-/// Application-level global variable to access the WebSockets
-///
-WebSocketsNotifications sockets = new WebSocketsNotifications();
+/// Global variable, the whole class
+/// is accessible by this global variable
+WebSocket socket = new WebSocket();
 
-///
-/// Put your WebSockets server IP address and port number
-///
-const String _SERVER_ADDRESS = "ws://192.168.1.45:34263";
+class WebSocket {
+  static final WebSocket _socketController = new WebSocket._internal();
 
-class WebSocketsNotifications {
-  static final WebSocketsNotifications _sockets = new WebSocketsNotifications._internal();
-
-  factory WebSocketsNotifications() {
-    return _sockets;
+  /// Reuse the same instance of websockets,
+  /// Called when Line 6 is called.
+  factory WebSocket() {
+    return _socketController;
   }
 
-  WebSocketsNotifications._internal();
+  WebSocket._internal();
+  
 
-  ///
-  /// The WebSocket "open" channel
-  ///
-  IOWebSocketChannel _channel;
+  // WebSocket channel
+  IO.Socket _channel;
 
-  ///
-  /// Is the connection established?
-  ///
+  // Is connection on?
   bool _isOn = false;
 
-  ///
-  /// Listeners
-  /// List of methods to be called when a new message
-  /// comes in.
-  ///
+  // Listeners to various events
   ObserverList<Function> _listeners = new ObserverList<Function>();
 
-  /// ----------------------------------------------------------
-  /// Initialization the WebSockets connection with the server
-  /// ----------------------------------------------------------
-  initCommunication() async {
-    ///
-    /// Just in case, close any previous communication
-    ///
-    reset();
+  /// ==========
+  /// Initialise communication
+  /// ==========
+  initCommunication() {
+    // reset previous communication, if any
+    // reset();
 
-    ///
-    /// Open a new WebSocket communication
-    ///
-    try {
-      _channel = new IOWebSocketChannel.connect("ws://localhost:34263");
+    // Initiate communication
+    _channel = IO.io('http://127.0.0.1:3000', <String, dynamic>{
+      'transports': ['websocket'],
+        // 'extraHeaders': {'foo': 'bar'} // optional
+    });
+    _channel.connect();
+    _isOn = true;
+    _channel.on('connect', (_) {
+      print('connected');
+      _channel.emit('msg', 'test');
+    });
+  }
 
-      ///
-      /// Start listening to new notifications / messages
-      ///
-      _channel.stream.listen(_onReceptionOfMessageFromServer);
-    } catch(e){
-      ///
-      /// General error handling
-      /// TODO
-      ///
-      print(e);
+  // Resets communication
+  reset() {
+    if (_channel != null) {
+      _channel.destroy();
+      _isOn = false;
     }
   }
 
-  /// ----------------------------------------------------------
-  /// Closes the WebSocket communication
-  /// ----------------------------------------------------------
-  reset(){
-    if (_channel != null){
-      if (_channel.sink != null){
-        _channel.sink.close();
-        _isOn = false;
-      }
+  // Sends a message to the server
+  send(String message, String data) {
+    if (_isOn == true) {
+      _channel.emit(message, data);
     }
   }
 
-  /// ---------------------------------------------------------
-  /// Sends a message to the server
-  /// ---------------------------------------------------------
-  send(String message){
-    if (_channel != null){
-      if (_channel.sink != null && _isOn){
-        _channel.sink.add(message);
-      }
-    }
-  }
-
-  /// ---------------------------------------------------------
+   /// ---------------------------------------------------------
   /// Adds a callback to be invoked in case of incoming
   /// notification
   /// ---------------------------------------------------------
