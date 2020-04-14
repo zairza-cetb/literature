4eaee9fac31675088cc3775e2b7ecdd32129971d
ankitjena <ankitjena13@gmail.com>
2020-04-14 14:50:30 +0530

    fix server listen bug

--- node_server/src/app.ts
+++ node_server/src/app.ts
@@ -1,11 +1,13 @@
-import express from 'express'
-import { createServer } from 'http'
-import socketio from 'socket.io'
-import { Room, GameStatus } from './models/Room'
-import mongoose from 'mongoose'
-import { MONGODB_URI, PORT } from './util/secrets'
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import socketio from "socket.io";
+import { Room, GameStatus } from "./models/Room";
+import mongoose from "mongoose";
+import { MONGODB_URI, PORT } from "./util/secrets";
 
-const app = express()
+const app = express();
+app.use(cors());
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true } ).then(
   () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
@@ -14,54 +16,54 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, use
   // process.exit();
 });
 
-const http = createServer(app)
+const http = createServer(app);
 
-const io = socketio(http)
+const io = socketio(http);
 
-app.get('/', function(req, res) {
-  res.send("Hello World")
-})
-app.set('port', PORT)
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
+app.set("port", PORT);
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   /**
    * Create a game, add a room in db with status "CREATED",
    * send the numerical roomId back in callback 
    * which will be used to join the game
    */
-  socket.on('create_game', async function(name, id, callback) {
+  socket.on("create_game", async function(name, id, callback) {
     const newRoom = new Room({
       status: GameStatus.CREATED,
       players: [{id, name}]
-    })
-    await newRoom.save()
+    });
+    await newRoom.save();
     // Create and join the socket room
-    socket.join(newRoom.roomId.toString())
-    callback(newRoom.roomId)
-  })
+    socket.join(newRoom.roomId.toString());
+    callback(newRoom.roomId);
+  });
 
   /**
    * Add the player to list of players in the room
    * Broadcast to other players about the player joined
    * Send other players details to the player
    */
-  socket.on('join_game', async function(roomId, name, id) {
-    const room = await Room.findOne({roomId})
+  socket.on("join_game", async function(roomId, name, id) {
+    const room = await Room.findOne({roomId});
     if(room.status === GameStatus.CREATED) {
       await Room.update(
         { roomId },
         { $push: { players: { name, id }}}
-      )
+      );
       // Send new player details to every other player in the room
-      socket.to(room.roomId.toString()).emit('new player', name)
+      socket.to(room.roomId.toString()).emit("new player", name);
       // Send other player details to the player
-      socket.emit('joined', room.players)
+      socket.emit("joined", room.players);
       //Join the room
-      socket.join(room.roomId.toString())
+      socket.join(room.roomId.toString());
     } else {
-      socket.emit('invalid room')
+      socket.emit("invalid room");
     }
-  })
-})
+  });
+});
 
-export default app
+export default http;
--- node_server/src/server.ts
+++ node_server/src/server.ts
@@ -1,22 +1,6 @@
-import errorHandler from "errorhandler";
+import server from "./app";
+import { PORT } from "./util/secrets";
 
-import app from "./app";
-
-/**
- * Error Handler. Provides full stack - remove for production
- */
-app.use(errorHandler());
-
-/**
- * Start Express server.
- */
-const server = app.listen(app.get("port"), () => {
-    console.log(
-        "  App is running at http://localhost:%d in %s mode",
-        app.get("port"),
-        app.get("env")
-    );
-    console.log("  Press CTRL-C to stop\n");
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
-
-export default server;
