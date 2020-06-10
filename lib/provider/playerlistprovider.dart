307a24bf9bc141fa4a07177c19479d64e9424c63
Aquib Baig <aquibbaig97@gmail.com>
2020-06-11 00:08:27 +0530

    create store tabs, in game currency

--- lib/provider/playerlistprovider.dart
+++ lib/provider/playerlistprovider.dart
@@ -8,7 +8,17 @@ class PlayerList extends ChangeNotifier {
   final List<Player> _currPlayer=[];
 
   UnmodifiableListView<Player> get players => UnmodifiableListView(_list);
+  double currency = 0.00;
   Player get currPlayer => _currPlayer.first;
+
+  void addCurrency(double c) {
+    currency += c;
+    currency = double.parse(currency.toStringAsFixed(2));
+  }
+
+  void deductCurrency (double c) {
+    currency -= c;
+  }
 
   void addPlayer(Player p){
     _list.add(p);
--- lib/screens/store.dart
+++ lib/screens/store.dart
@@ -1,32 +1,108 @@
 import 'package:flutter/material.dart';
-import 'package:literature/components/appbar.dart';
+import 'package:literature/provider/playerlistprovider.dart';
+import 'package:literature/utils/constants.dart';
+import 'package:provider/provider.dart';
 
 class Store extends StatefulWidget {
   _StoreState createState() => _StoreState();
 }
 
 class _StoreState extends State<Store> {
+  List moneyValues;
+  // selected money value for purchase.
+  double amountOfPurchase;
+  void initState() {
+    super.initState();
+    moneyValues = new List();
+    moneyOptions.forEach((element) {
+      moneyValues.add(element);
+    });
+  }
   @override
   Widget build(BuildContext context) {
+    final playerProvider = Provider.of<PlayerList>(context, listen: false);
     return new SafeArea(
       bottom: false,
       top: false,
-      child: Scaffold(
-        // Disable going to the waiting page
-        // cause there won't be any forwarding
-        // from there on.
-        // appBar: new AppBar(),
-        appBar: AppBar(
-          title: new Text("Store", style: TextStyle(color: Color(0xFF303f9f))),
-          elevation: 0,
-          leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: Icon(Icons.arrow_back_ios),
-          color: Color(0xFF303f9f),
-        ),
-          backgroundColor: Colors.lightBlue[100],
+      child: DefaultTabController(
+        length: 3,
+          child: Scaffold(
+          // Disable going to the waiting page
+          // cause there won't be any forwarding
+          // from there on.
+          // appBar: new AppBar(),
+          appBar: AppBar(
+            title: new Text("Literature Store"),
+            actions: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(2),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Row(children: <Widget>[
+                    Icon(Icons.attach_money, size: 14),
+                    Text(playerProvider.currency.toString(), style: TextStyle(fontStyle: FontStyle.italic)),
+                  ],)
+                ),
+              )
+            ],
+            bottom: TabBar(tabs: [
+              Tab(text: "Coins"),
+              Tab(text: "Visuals"),
+              Tab(text: "Combos")
+            ]),
+            elevation: 0,
+            leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(Icons.arrow_back_ios),
+          ),
+            backgroundColor: Colors.lightBlue[800],
+          ),
+          body: TabBarView(children: [
+            // Currency
+            GridView.count(
+              crossAxisCount: 2,
+              children: moneyOptions.map((e) => Padding(
+                padding: const EdgeInsets.all(4),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2.0
+                    )
+                  ),
+                  padding: const EdgeInsets.all(8),
+                  // color: Colors.transparent,
+                  child: GestureDetector(
+                    onTap: () {
+                      amountOfPurchase = double.parse(e);
+                      final playerProvider = Provider.of<PlayerList>(context, listen: false);
+                      playerProvider.addCurrency(amountOfPurchase);
+                      setState(() {});
+                    },
+                    child: Stack(children: <Widget>[
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: Container(height: 140,child: Hero(tag: e, child: Image.asset("assets/money.png"))),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.attach_money), Text(e, style: TextStyle(fontSize: 30, fontStyle: FontStyle.italic))
+                          ],
+                        )
+                      )
+                    ],),
+                  ),
+                ),
+              )).toList(),
+            ),
+            Text("Buy In game mats and cards"),
+            Text("Game packs"),
+          ]),
         ),
       ),
     );
