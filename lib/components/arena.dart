40fa72c8ecd94aba2f712896d3105cb399adb1cc
Aquib Baig <aquibbaig97@gmail.com>
2020-06-29 20:55:46 +0530

    arena score update (#123)

--- lib/components/arena.dart
+++ lib/components/arena.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Arena extends StatefulWidget {
-  Arena(this.turnsMapper, this.height, this.messages);
+  Arena(this.turnsMapper, this.height, this.messages, this.currTeamScore, this.opponentTeamScore);
   final turnsMapper;
   double height;
   final List messages;
+  int currTeamScore;
+  int opponentTeamScore;
   _ArenaState createState() => _ArenaState();
 }
 
@@ -33,6 +35,7 @@ class _ArenaState extends State<Arena> {
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width= MediaQuery.of(context).size.width;
+    var scoreCard = widget.currTeamScore.toString() + "-" + widget.opponentTeamScore.toString();
     List<Widget> arenaMessages = widget.messages.map((m) {
       return Container(
         child: Text(m, style: TextStyle(color: Colors.white, fontFamily: 'VT323', fontSize: height*0.0223)),
@@ -51,7 +54,17 @@ class _ArenaState extends State<Arena> {
         children: <Widget>[
           Container(
             margin: EdgeInsets.fromLTRB(0, 0, 0, height*0.01116),
-            child: Text(score, style: TextStyle(color: Colors.white, fontSize: height*0.0335, fontFamily: 'Montserrat')),
+            // child: Text(scoreCard, style: TextStyle(color: Colors.white, fontSize: height*0.0335, fontFamily: 'Montserrat')),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("HOME ", style: TextStyle(color: Colors.white, fontSize: height*0.0135, fontFamily: 'Montserrat')),
+                Text(widget.currTeamScore.toString(), style: TextStyle(color: Colors.white, fontSize: height*0.0335, fontFamily: 'Montserrat')),
+                Text(" / ", style: TextStyle(color: Colors.white, fontSize: height*0.0335, fontFamily: 'Montserrat')),
+                Text(widget.currTeamScore.toString(), style: TextStyle(color: Colors.white, fontSize: height*0.0335, fontFamily: 'Montserrat')),
+                Text(" AWAY", style: TextStyle(color: Colors.white, fontSize: height*0.0135, fontFamily: 'Montserrat')),
+              ]
+            ),
           ),
           Expanded(
             child: Column(
