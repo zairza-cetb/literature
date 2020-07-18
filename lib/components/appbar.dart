052d3084ccbbee53c0397bfecb910b821b5fe771
moby-dicc <animeshk503@gmail.com>
2020-07-18 23:47:18 +0530

    Added the menu

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:literature/utils/game_communication.dart';
+import 'package:literature/components/credits.dart';
 
 class GlobalAppBar extends StatefulWidget implements PreferredSizeWidget  {
   @override
@@ -8,7 +9,354 @@ class GlobalAppBar extends StatefulWidget implements PreferredSizeWidget  {
   @override
   Size get preferredSize => new Size.fromHeight(kToolbarHeight);
 }
+showAboutDialog(BuildContext context) {  
 
+  Widget title = Container( 
+    margin: const EdgeInsets.all(10.0),
+    alignment: Alignment.center, 
+    child: Text(
+      "About"
+      ),  
+  );  
+  Widget devdet=Container(
+        margin: const EdgeInsets.all(2.0),
+    decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(10.00),
+      border: Border.all( 
+        color: Color(0xFF6ad1ff)
+        )
+      ),
+    child:GestureDetector(
+      child:Container(
+        height: 50.0,
+        child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        shadowColor: Color(0xFF6ad1ff),
+        //color: Color(0xFF039be5),
+        elevation: 7.0,
+        child: Center(
+          child: Text(
+            "Developers' Details",
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'B612'
+              ),
+            ),
+         ),
+        ),
+      ),  
+      onTap: () {  
+        Navigator.push(context,MaterialPageRoute(builder: 
+        (context) => Credits()
+        ));
+     }, 
+    ), 
+  );
+  Widget lic=Container(
+        margin: const EdgeInsets.all(2.0),
+    decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(10.00),
+      border: Border.all( 
+        color: Color(0xFF6ad1ff)
+      ),
+    ),
+    child:GestureDetector(
+      child:Container(
+        height: 50.0,
+        child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        shadowColor: Color(0xFF6ad1ff),
+        //color: Color(0xFF039be5),
+        elevation: 7.0,
+        child: Center(
+          child: Text(
+            'License',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'B612'
+              ),
+            ),
+         ),
+        ),
+      ),  
+      onTap: () {  
+      Navigator.of(context).pop();  
+     }, 
+    ), 
+  );
+  // Create AlertDialog  
+  AlertDialog alert = AlertDialog(  
+    actions: [  
+      title, devdet, lic,
+    ],  
+  );  
+  // show the dialog  
+  showDialog(  
+    context: context,  
+    builder: (BuildContext context) {  
+      return alert;  
+    },  
+  );  
+}  
+showSettingsDialog(BuildContext context) {  
+Widget title = Container( 
+    margin: const EdgeInsets.all(15.0),
+    alignment: Alignment.center, 
+    child: Text(
+      "Settings"
+      ),  
+  );  
+  Widget music=Container(
+      margin: const EdgeInsets.all(2.0),
+      decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(10.00),
+      border: Border.all( 
+        color: Color(0xFF6ad1ff)
+        )
+      ),
+        child:Container(
+          height: 50.0,
+          width: 260.0,
+          child: Material(
+            borderRadius: BorderRadius.circular(10.0),
+            shadowColor: Color(0xFF6ad1ff),
+        //color: Color(0xFF039be5),
+        elevation: 7.0,
+              child: Stack(
+                children:<Widget>[
+                  Container(
+                    //color: Colors.green,
+                    height: 30.0,
+                    width: 50.0,
+                    margin: const EdgeInsets.fromLTRB(40.0,10.0,20.0,10.0),
+                    alignment: Alignment(0.0,0.0),
+                    child:Text(
+                      'Music',
+                      style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'B612'
+                          ),
+                        ),
+                    ),
+                  GestureDetector(
+                    child:Container(
+                      //color: Colors.blueGrey,
+                      height: 30.0,
+                      width: 50.0,
+                      margin: const EdgeInsets.fromLTRB(150.0,10.0,20.0,10.0),
+                      alignment: Alignment(1.0,0.0),
+                      child:FlatButton(
+                        child: Text("On"
+                          ),
+                        onPressed: null,
+                        ),
+                      ),
+                    onTap: () {
+                       Navigator.of(context).pop();   //Insert Audio and toggle
+                       } ,
+                    ), 
+                ]
+              ),           
+          ),
+        ),   
+  );
+  Widget theme=Container(
+    margin: const EdgeInsets.all(2.0),
+    decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(10.00),
+      border: Border.all( 
+        color: Color(0xFF6ad1ff)
+        )
+      ), //             <--- BoxDecoration here
+    child:GestureDetector(
+      child:Container(
+        height: 50.0,
+        child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        shadowColor: Color(0xFF6ad1ff),
+        //color: Color(0xFF039be5),
+        elevation: 7.0,
+        child: Center(
+          child: Text(
+            'Change Theme',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'B612'
+              ),
+            ),
+         ),
+        ),
+      ),  
+      onTap: () {  
+      Navigator.of(context).pop();  
+     }, 
+    ), 
+  );
+  // Create AlertDialog  
+  AlertDialog alert = AlertDialog(  
+    actions: [  
+      title, music, theme,
+    ],  
+  );  
+  // show the dialog  
+  showDialog(  
+    context: context,  
+    builder: (BuildContext context) {  
+      return alert;  
+    },  
+  );  
+}  
+ showAlertDialog(BuildContext context) {  
+  // Create button  
+  Widget title = Container( 
+    margin: const EdgeInsets.all(15.0),
+    alignment: Alignment.center, 
+    child: Text(
+      "Options"
+      ),  
+  );  
+  Widget settings=Container(
+        margin: const EdgeInsets.all(2.0),
+    decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(10.00),
+      border: Border.all( 
+        color: Color(0xFF6ad1ff)
+        )
+      ),
+    child:GestureDetector(
+      child:Container(
+        height: 50.0,
+        child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        shadowColor: Color(0xFF6ad1ff),
+        //color: Color(0xFF039be5),
+        elevation: 7.0,
+        child: Center(
+          child: Text(
+            'Settings',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'B612'
+              ),
+            ),
+         ),
+        ),
+      ),  
+      onTap: () {  
+      showSettingsDialog(context);  
+     }, 
+    ), 
+  );
+  Widget profile=Container(
+        margin: const EdgeInsets.all(2.0),
+    decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(10.00),
+      border: Border.all( 
+        color: Color(0xFF6ad1ff)
+        )
+      ),
+    child:GestureDetector(
+      child:Container(
+        height: 50.0,
+        child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        shadowColor: Color(0xFF6ad1ff),
+        //color: Color(0xFF039be5),
+        elevation: 7.0,
+        child: Center(
+          child: Text(
+            'Profile',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'B612'
+              ),
+            ),
+         ),
+        ),
+      ),  
+      onTap: () {  
+      Navigator.of(context).pop();  
+     }, 
+    ), 
+  );
+    Widget about=Container(
+        margin: const EdgeInsets.all(2.0),
+    decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(10.00),
+      border: Border.all( 
+        color: Color(0xFF6ad1ff)
+        )
+      ),
+    child:GestureDetector(
+      child:Container(
+        height: 50.0,
+        child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        shadowColor: Color(0xFF6ad1ff),
+        //color: Color(0xFF039be5),
+        elevation: 7.0,
+        child: Center(
+          child: Text(
+            'About',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'B612'
+              ),
+            ),
+         ),
+        ),
+      ),  
+      onTap: () {  
+      showAboutDialog(context);  
+     }, 
+    ), 
+  );
+  Widget howtoplay=Container(
+    margin: const EdgeInsets.all(2.0),
+    decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(10.00),
+      border: Border.all( 
+        color: Color(0xFF6ad1ff)
+        )
+      ), //             <--- BoxDecoration here
+    child:GestureDetector(
+      child:Container(
+        height: 50.0,
+        child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        shadowColor: Color(0xFF6ad1ff),
+        //color: Color(0xFF039be5),
+        elevation: 7.0,
+        child: Center(
+          child: Text(
+            'How To Play',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'B612'
+              ),
+            ),
+         ),
+        ),
+      ),  
+      onTap: () {  
+      Navigator.of(context).pop();  
+     }, 
+    ), 
+  );
+  // Create AlertDialog  
+  AlertDialog alert = AlertDialog(  
+    actions: [  
+      title, howtoplay, settings, profile, about,
+    ],  
+  );  
+  // show the dialog  
+  showDialog(  
+    context: context,  
+    builder: (BuildContext context) {  
+      return alert;  
+    },  
+  );  
+}  
 class _GlobalAppBarState extends State<GlobalAppBar> {
   @override
   Widget build(BuildContext context) {
@@ -45,6 +393,20 @@ class _GlobalAppBarState extends State<GlobalAppBar> {
         //     color: Color(0xFF303f9f),
         //   ),
         // ],
+        actions:<Widget>[ 
+          GestureDetector(
+            child: CircleAvatar(
+              radius: 20,  
+              backgroundColor: Color(0xFF6ad1ff),
+/*              child: Text(
+                'OK'
+              ),*/
+              ),
+            onTap: () {
+            showAlertDialog(context);  
+            },
+          ),
+        ],
       ),
     );
   }
---
+++ lib/components/credits.dart
@@ -0,0 +1,318 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+    
+
+class Credits extends StatelessWidget {
+  @override
+Widget build(BuildContext context){  
+   return SingleChildScrollView(
+   child:Container(
+     color: Colors.white,
+     child:Column( 
+    children: <Widget>[
+      Container(
+        color: Colors.white,
+        height: 100,
+    margin: const EdgeInsets.all(15.0),
+    alignment: Alignment.center, 
+    child: Text(
+      "Developers",
+      style:TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.w100, 
+        fontFamily: 'B612',
+        color: Colors.black
+        ),
+      ),  
+    ),
+    Center( 
+        child:Container(
+          margin: const EdgeInsets.all(2.0),
+          decoration:BoxDecoration(
+            borderRadius: BorderRadius.circular(10.00),
+            border: Border.all( 
+              color: Colors.blueAccent
+                )
+              ),     
+            child:Container(
+              height: 100.0,
+              width: 100.0,
+              child: Material(
+              borderRadius: BorderRadius.circular(10.0),
+              shadowColor: Color(0xFFb3e5fc),
+              //color: Color(0xFF039be5),
+              elevation: 7.0,
+                  child:GestureDetector(
+                    onTap: (){
+                      launch('https://zairza.in/');
+                          },
+                    child: Container(                 
+                    //margin:EdgeInsets.fromLTRB(1.0,10.0,150.0,10.0) ,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                        'assets/zairza_logo.jpg'),
+                        fit: BoxFit.fill
+                            ),
+                            borderRadius: BorderRadius.circular(10),             
+                    )
+                    )
+                ),
+              ),
+              ),  
+          ), 
+        ),  
+      Container( 
+          color: Colors.white,
+          margin: const EdgeInsets.all(15.0),
+          alignment: Alignment.center, 
+          child: Text(
+            "ZAIRZA, a society, a cult for CETIANS who want to be ahead in the game. The club is managed by its students and alumni. The society was founded in the year 2005 and got officially recognised in the year 2007 as a technical society of CET.",
+            style:TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w100, 
+              fontFamily: 'B612',
+              color: Colors.black
+              ),
+            ), 
+          ),  
+      
+  SingleChildScrollView(
+    child: Column(
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.all(2.0),
+          decoration:BoxDecoration(
+            borderRadius: BorderRadius.circular(10.00),
+            border: Border.all( 
+              color: Colors.blueAccent
+              )
+            ),
+            child:Container(
+              height: 50.0,
+              child: Material(
+              borderRadius: BorderRadius.circular(10.0),
+              shadowColor: Color(0xFFb3e5fc),
+              //color: Color(0xFF039be5),
+              elevation: 7.0,
+              child: Center(
+                child: Stack(
+                children:<Widget> [
+                  Container(
+                    width: 150,
+                    //color: Colors.amber,
+                    margin: EdgeInsets.fromLTRB(70.0,15.80,20.0,5.0),
+                  child:Text(
+                  'Aquib Baig',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'B612'
+                    ),
+                  ),
+                  ),
+                  Align(
+              alignment: Alignment.centerLeft,
+            child:Container(
+              margin: EdgeInsets.only(left:16),
+              child:CircleAvatar(
+                radius: 20,
+                //backgroundColor: Colors.lime,
+                ),
+              ),
+            ),
+                  ]
+                ),
+              ),
+              ),  
+          ),
+              ),
+            ]
+          ), 
+        ),
+  Container(
+    margin: const EdgeInsets.all(2.0),
+    decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(10.00),
+      border: Border.all( 
+        color: Colors.blueAccent
+        )
+      ),
+      child:Container(
+        height: 50.0,
+        child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        shadowColor: Color(0xFFb3e5fc),
+        //color: Color(0xFF039be5),
+        elevation: 7.0,
+        child: Center(
+          child: Stack(
+          children:<Widget> [
+            Container(
+              width: 150,
+              //color: Colors.amber,
+              margin: EdgeInsets.fromLTRB(69.0,15.80,20.0,5.0),
+            child:Text(
+            'Harish Umaid',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'B612'
+              ),
+            ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+            child:Container(
+              margin: EdgeInsets.only(left:16),
+              child:CircleAvatar(
+                radius: 20,
+                //backgroundColor: Colors.lime,
+                ),
+              ),
+            ),
+            ]
+          ),
+         ),
+        ),  
+    ), 
+  ),
+  Container(
+    margin: const EdgeInsets.all(2.0),
+    decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(10.00),
+      border: Border.all( 
+        color: Colors.blueAccent
+        )
+      ),
+      child:Container(
+        height: 50.0,
+        child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        shadowColor: Color(0xFFb3e5fc),
+        //color: Color(0xFF039be5),
+        elevation: 7.0,
+        child: Center(
+          child: Stack(
+          children:<Widget> [
+            Container(
+              width: 150,
+              //color: Colors.amber,
+              margin: EdgeInsets.fromLTRB(70.0,15.80,20.0,5.0),
+            child:Text(
+            'Animesh Kar',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'B612'
+              ),
+            ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+            child:Container(
+              margin: EdgeInsets.only(left:16),
+              child:CircleAvatar(
+                radius: 20,
+                //backgroundColor: Colors.lime,
+                ),
+              ),
+            ),
+            ]
+          ),
+         ),
+        ),  
+    ), 
+  ),
+  Container(
+    margin: const EdgeInsets.all(2.0),
+    decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(10.00),
+      border: Border.all( 
+        color: Colors.blueAccent
+        )
+      ),
+      child:Container(
+        height: 50.0,
+        child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        shadowColor: Color(0xFFb3e5fc),
+        //color: Color(0xFF039be5),
+        elevation: 7.0,
+        child: Center(
+          child: Stack(
+          children:<Widget> [
+            Container(
+              width: 150,
+              //color: Colors.amber,
+              margin: EdgeInsets.fromLTRB(70.0,15.80,20.0,5.0),
+            child:Text(
+            'Prateek Mohanty',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'B612'
+              ),
+            ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+            child:Container(
+              margin: EdgeInsets.only(left:16),
+              child:CircleAvatar(
+                radius: 20,
+                //backgroundColor: Colors.lime,
+                ),
+              ),
+            ),
+            ]
+          ),
+         ),
+        ),  
+    ), 
+  ),
+ /* Container(
+    margin: const EdgeInsets.all(2.0),
+    decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(10.00),
+      border: Border.all( 
+        color: Colors.blueAccent
+        )
+      ),
+      child:Container(
+        height: 50.0,
+        child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        shadowColor: Color(0xFFb3e5fc),
+        //color: Color(0xFF039be5),
+        elevation: 7.0,
+        child: Center(
+          child: Stack(
+          children:<Widget> [
+            Container(
+              width: 150,
+              color: Colors.amber,
+              margin: EdgeInsets.fromLTRB(70.0,15.80,20.0,5.0),
+            child:Text(
+            '5',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'B612'
+              ),
+            ),
+            ),
+            Container(
+              margin:EdgeInsets.fromLTRB(10.0,10.0,20.0,10.0) ,
+              child:CircleAvatar(
+                radius: 22,
+                backgroundColor: Colors.lime,
+                ),
+              ),
+            ]
+          ),
+         ),
+        ),  
+    ), 
+  ),*/  
+    ],  
+  ),
+   ),
+   );  
+} 
+}
