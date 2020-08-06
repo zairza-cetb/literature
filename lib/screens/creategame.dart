3a8a5b4f61a6a2a12a278538b36d388a633a8eaf
StrangeNoob <sipun63427452@gmail.com>
2020-08-07 04:45:30 +0530

    Add MediaQuery in the Padding and Margin

--- lib/screens/creategame.dart
+++ lib/screens/creategame.dart
@@ -338,152 +338,173 @@ class _CreateGame extends State<CreateGame> with AfterLayoutMixin<CreateGame> {
               child: Loader(),
             )
           : Center(
-              child: new ListView(children: <Widget>[
-                Stack(children: <Widget>[
-                  Container(
-                      height: MediaQuery.of(context).size.height - 100,
-                      width: MediaQuery.of(context).size.width,
-                      color: Colors.transparent),
-                  Positioned(
-                      top: MediaQuery.of(context).size.height / 15,
-                      child: Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(45.0),
-                                topRight: Radius.circular(45.0),
-                              ),
-                              color: Colors.white),
-                          height: MediaQuery.of(context).size.height -
-                              MediaQuery.of(context).size.height / 15,
-                          width: MediaQuery.of(context).size.width)),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+              child: new ListView(
+                children: <Widget>[
+                  Stack(
                     children: <Widget>[
-                      Center(
-                        child: new Container(
-                          margin: EdgeInsets.only(top: 10.0),
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage('assets/create_room_bg.png'),
-                                fit: BoxFit.cover),
-                          ),
-                          height: 339.0,
-                          width: 241.0,
-                        ),
-                      ),
                       Container(
-                        padding:
-                            EdgeInsets.only(top: 30.0, left: 30.0, right: 30.0),
-                        child: Column(
-                          children: <Widget>[
-                            InkWell(
-                                onTap: () => (user != null)
-                                    ? _onCreateGame()
-                                    : Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                CreateRoom())),
-                                child: Container(
-                                  height: 50.0,
-                                  child: Material(
-                                    borderRadius: BorderRadius.circular(20.0),
-                                    shadowColor: Color(0xFFb3e5fc),
-                                    color: Color(0xFF039be5),
-                                    elevation: 7.0,
-                                    child: Center(
-                                      child: Text(
-                                        'Create Room',
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                            fontFamily: 'B612'),
-                                      ),
-                                    ),
+                          height: MediaQuery.of(context).size.height - 100,
+                          width: MediaQuery.of(context).size.width,
+                          color: Colors.transparent),
+                      Positioned(
+                          top: MediaQuery.of(context).size.height / 15,
+                          child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(45.0),
+                                    topRight: Radius.circular(45.0),
                                   ),
-                                )),
-                            SizedBox(height: 20.0),
-                            InkWell(
-                                onTap: () => (user != null)
-                                    ? showJoinDialog(context)
-                                    : Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => JoinRoom())),
-                                child: Container(
-                                  height: 50.0,
-                                  child: Material(
-                                    borderRadius: BorderRadius.circular(20.0),
-                                    shadowColor: Color(0xFF24315E),
-                                    color: Color(0xffb0bec5),
-                                    elevation: 7.0,
-                                    child: Center(
-                                      child: Text(
-                                        'Join Room',
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                            fontFamily: 'B612'),
-                                      ),
-                                    ),
-                                  ),
-                                )),
-                            SizedBox(height: 50.0),
-                            InkWell(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => JoinRoom()),
-                                  );
-                                },
-                                child: Container(
-                                  height: 40.0,
-                                  color: Colors.transparent,
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        border: Border.all(
-                                            color: Colors.black,
-                                            style: BorderStyle.solid,
-                                            width: 1.0),
-                                        color: Colors.transparent,
+                                  color: Colors.white),
+                              height: MediaQuery.of(context).size.height -
+                                  MediaQuery.of(context).size.height / 15,
+                              width: MediaQuery.of(context).size.width)),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Center(
+                            child: new Container(
+                              margin: EdgeInsets.only(
+                                  top: MediaQuery.of(context).size.height *
+                                      0.01),
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image:
+                                        AssetImage('assets/create_room_bg.png'),
+                                    fit: BoxFit.cover),
+                              ),
+                              height: 339.0,
+                              width: 241.0,
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(
+                              top: MediaQuery.of(context).size.height * 0.03,
+                              left: MediaQuery.of(context).size.width * 0.03,
+                              right: MediaQuery.of(context).size.width * 0.03,
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                InkWell(
+                                    onTap: () => (user != null)
+                                        ? _onCreateGame()
+                                        : Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    CreateRoom())),
+                                    child: Container(
+                                      height: 50.0,
+                                      child: Material(
                                         borderRadius:
-                                            BorderRadius.circular(20.0)),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        Center(
-                                          child: ImageIcon(AssetImage(
-                                              'assets/facebook.png')),
-                                        ),
-                                        SizedBox(width: 10.0),
-                                        Center(
-                                          child: InkWell(
-                                            onTap: () {
-                                              var auth = FirebaseAuth.instance;
-                                              auth.signOut();
-                                            },
-                                            child: Text(
-                                              'Invite Friends with Facebook',
-                                              style: TextStyle(
-                                                  fontWeight: FontWeight.bold,
-                                                  fontFamily: 'B612'),
-                                            ),
+                                            BorderRadius.circular(20.0),
+                                        shadowColor: Color(0xFFb3e5fc),
+                                        color: Color(0xFF039be5),
+                                        elevation: 7.0,
+                                        child: Center(
+                                          child: Text(
+                                            'Create Room',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold,
+                                                fontFamily: 'B612'),
                                           ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                )),
-                          ],
-                        ),
+                                        ),
+                                      ),
+                                    )),
+                                SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.02),
+                                InkWell(
+                                    onTap: () => (user != null)
+                                        ? showJoinDialog(context)
+                                        : Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    JoinRoom())),
+                                    child: Container(
+                                      height: 50.0,
+                                      child: Material(
+                                        borderRadius:
+                                            BorderRadius.circular(20.0),
+                                        shadowColor: Color(0xFF24315E),
+                                        color: Color(0xffb0bec5),
+                                        elevation: 7.0,
+                                        child: Center(
+                                          child: Text(
+                                            'Join Room',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold,
+                                                fontFamily: 'B612'),
+                                          ),
+                                        ),
+                                      ),
+                                    )),
+                                SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.05),
+                                InkWell(
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => JoinRoom()),
+                                      );
+                                    },
+                                    child: Container(
+                                      height: 40.0,
+                                      color: Colors.transparent,
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                            border: Border.all(
+                                                color: Colors.black,
+                                                style: BorderStyle.solid,
+                                                width: 1.0),
+                                            color: Colors.transparent,
+                                            borderRadius:
+                                                BorderRadius.circular(20.0)),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: <Widget>[
+                                            Center(
+                                              child: ImageIcon(
+                                                AssetImage(
+                                                    'assets/facebook.png'),
+                                              ),
+                                            ),
+                                            SizedBox(width: 10.0),
+                                            Center(
+                                              child: InkWell(
+                                                onTap: () {
+                                                  var auth =
+                                                      FirebaseAuth.instance;
+                                                  auth.signOut();
+                                                },
+                                                child: Text(
+                                                  'Invite Friends with Facebook',
+                                                  style: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      fontFamily: 'B612'),
+                                                ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ]),
-              ]),
+                ],
+              ),
             ),
     );
   }
